33051071a6bdebaa78d5923f151eec19194020a0
anderson <nobreanderson@hotmail.com>
2025-08-19 19:58:18 -0300

    Criar modelos Post e Comentario no app Blog

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+    data_publicacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
+class Comentario(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comentarios")
+    autor = models.CharField(max_length=100)
+    texto = models.TextField()
+    data_comentario = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.autor} - {self.post.titulo}"
