8d62c20c73627435e723aede952c1641a8c6e73b
anderson <nobreanderson@hotmail.com>
2025-08-21 18:46:54 -0300

    Adicionando Views

---
+++ Blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import post_view, home_view
+
+urlpatterns = [
+    path('', home_view, name='home'),  # rota raiz
+    path('post/', post_view, name='post')
+]
